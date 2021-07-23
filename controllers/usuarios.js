8222284f025cfbf9468fe0a343cc94aae1ddf6b6
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2021-07-23 11:21:50 -0500

    Autenticación JWT - 23-07-2021

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -75,9 +75,11 @@ const usuariosDelete = async(req, res) => {
     // const usuario = await Usuario.findByIdAndDelete( id );
 
     const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+    // const usuarioAutenticado = req.usuario; // identifica el usuario autenticado
 
     res.status(200).json({
         usuario
+        // usuarioAutenticado
     });
 };
 
